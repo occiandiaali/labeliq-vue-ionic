670cc5e4ba655e56330751739904ea54a37c635d
occiandiaali <occiandiaali@gmail.com>
2025-12-24 10:37:32 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'labeliq',
+  webDir: 'dist'
+};
+
+export default config;
