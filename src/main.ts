9034d5f20bf1d63884ced6504b362c836e5fa3d9
occiandiaali <occiandiaali@gmail.com>
2025-12-26 09:18:09 +0100

    Move element loader after app mount

--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,10 @@ import "@ionic/vue/css/palettes/dark.system.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-// Call element loader
-defineCustomElements(window);
-
 const app = createApp(App).use(IonicVue).use(router);
 
 router.isReady().then(() => {
   app.mount("#app");
+  // Call element loader
+  defineCustomElements(window);
 });
